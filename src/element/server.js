e698a380d26d114def16f427f8b41a9df9fa0eb7
Arlo Watts <arlowatts1@gmail.com>
2024-08-17 19:36:33 -0700

    Add Endpoints to Server

--- src/element/server.js
+++ src/element/server.js
@@ -1,4 +1,6 @@
 import { Element } from "./element.js";
+import { Output } from "./output.js";
+import { Input } from "./input.js";
 
 const SERVER_WIDTH = 150;
 const SERVER_HEIGHT = 100;
@@ -7,5 +9,8 @@ export class Server extends Element {
     constructor(name, x, y) {
         super(name, x, y, SERVER_WIDTH, SERVER_HEIGHT, true);
         this.type += ".Server";
+
+        this.endpoints.push(new Output(this.width * 0.5, 0, this));
+        this.endpoints.push(new Input(this.width * 0.5, this.height, this));
     }
 }
