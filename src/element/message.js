af9d3d889a540626fbe16288cdd2736fee331a61
Arlo Watts <arlowatts1@gmail.com>
2024-08-17 23:51:35 -0700

    Draw message as it moves over Route

--- src/element/message.js
+++ src/element/message.js
@@ -1,7 +1,7 @@
 import { Element } from "./element.js";
+import { Route } from "./route.js";
 
-const MESSAGE_WIDTH = 10;
-const MESSAGE_HEIGHT = 10;
+const MESSAGE_RADIUS = 10;
 
 export class Message extends Element {
     time = 0;
@@ -15,8 +15,31 @@ export class Message extends Element {
         super("Message", 0, 0, -1, -1, false);
         this.type += ".Message";
 
+        this.time = Date.now();
+
         this.origin = origin;
         this.parentElement = origin;
+    }
+
+    draw(ctx, Styles) {
+        if (this.parentElement.type === "Element.Route") {
+            let pointOnRoute = this.parentElement.getPointOnRoute((Date.now() - this.time) / Route.delay);
+
+            if (pointOnRoute) {
+                ctx.save();
+
+                // draw the circle
+                Styles.box(ctx);
+                ctx.beginPath();
+
+                ctx.arc(pointOnRoute.x, pointOnRoute.y, MESSAGE_RADIUS, 0, 2 * Math.PI);
+
+                ctx.fill();
+                ctx.stroke();
+
+                ctx.restore();
+            }
+        }
     }
 
     set(element) {
--- src/element/route.js
+++ src/element/route.js
@@ -1,6 +1,8 @@
 import { Element } from "./element.js";
 
 export class Route extends Element {
+    static delay = 1000;
+
     endpointA = null;
     endpointB = null;
 
@@ -16,17 +18,44 @@ export class Route extends Element {
         let previousElement = message.parentElement;
         message.set(this);
 
-        if (this.endpointA.type === this.endpointB.type) {
-            message.fail();
+        message.time = Date.now();
+
+        setTimeout(() => {
+            if (this.endpointA.type === this.endpointB.type) {
+                message.fail();
+            }
+            else if (this.endpointA === previousElement && this.endpointB) {
+                this.endpointB.addMessage(message);
+            }
+            else if (this.endpointB === previousElement && this.endpointA) {
+                this.endpointA.addMessage(message);
+            }
+            else {
+                message.fail();
+            }
+        }, Route.delay);
+    }
+
+    getPointOnRoute(t) {
+        let aX = this.endpointA.x + this.endpointA.width * 0.5;
+        let aY = this.endpointA.y + this.endpointA.height * 0.5;
+        let bX = this.endpointB.x + this.endpointB.width * 0.5;
+        let bY = this.endpointB.y + this.endpointB.height * 0.5;
+
+        if (this.endpointA.type === "Element.Endpoint.Output" && this.endpointB.type === "Element.Endpoint.Input") {
+            return {
+                x: aX + t * (bX - aX),
+                y: aY + t * (bY - aY),
+            };
         }
-        else if (this.endpointA === previousElement && this.endpointB) {
-            this.endpointB.addMessage(message);
-        }
-        else if (this.endpointB === previousElement && this.endpointA) {
-            this.endpointA.addMessage(message);
+        else if (this.endpointA.type === "Element.Endpoint.Input" && this.endpointB.type === "Element.Endpoint.Output") {
+            return {
+                x: bX + t * (aX - bX),
+                y: bY + t * (aY - bY),
+            };
         }
         else {
-            message.fail();
+            return null;
         }
     }
 
