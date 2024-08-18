94dbf5e966d5b998be7abdf3838b1b33a5c8573b
Arlo Watts <arlowatts1@gmail.com>
2024-08-18 11:12:50 -0700

    Preserve pane position

--- src/element/pane.js
+++ src/element/pane.js
@@ -9,8 +9,11 @@ const PANE_WIDTH = (window.innerWidth - (PANE_WIDTH_RATIO + 1) * PADDING) / PANE
 const PANE_HEIGHT = (window.innerHeight - (PANE_HEIGHT_RATIO + 1) * PADDING) / PANE_HEIGHT_RATIO;
 
 export class Pane extends Element {
+    static x = PADDING;
+    static y = PADDING;
+
     constructor(parentElement) {
-        super(parentElement.name, PADDING, PADDING, PANE_WIDTH, PANE_HEIGHT, true, false);
+        super(parentElement.name, Pane.x, Pane.y, PANE_WIDTH, PANE_HEIGHT, true, false);
         this.type += ".Pane";
 
         this.metrics = parentElement.metrics;
@@ -18,5 +21,12 @@ export class Pane extends Element {
 
     draw(ctx, Styles) {
         super.draw(ctx, Styles);
+    }
+
+    move(movementX, movementY) {
+        super.move(movementX, movementY);
+
+        Pane.x = this.x;
+        Pane.y = this.y;
     }
 }
